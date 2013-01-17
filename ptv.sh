b32371c2e22db71b1717ba979421f997d7e58eea
Maciej Klemarczyk <mklemarczyk@sigma.ug.edu.pl>
2013-01-17 13:35:31 +0100

    Puste funkcje :)

--- ptv.sh
+++ ptv.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+function wyborProgramu {
+
+}
+
+function pobierzProgram {
+	wget -O "program.tmp" "http://tv.wp.pl/id,$1,d,$2,all,1,mprogramy.html";
+	rm "program.tmp";
+}
+
+function wyswietlenieProgramu {
+
+}
+
+
 dialog --msgbox "Witaj w programie TV" 0 0;
 
 dialog --menu "Wybierz program:" 0 0 0 1 "TVP-1" 2 "TVP-2" 5 "Polsat" 17 "TVN";
-
-wget -O "program.tmp" "http://tv.wp.pl/id,1,d,2013-01-17,all,1,mprogramy.html";
 
 
