ea917bbffc9e3b291f7afbc87407aa1979ee46c2
Maciej Klemarczyk <mklemarczyk@sigma.ug.edu.pl>
2013-01-17 14:04:36 +0100

    Poprawa cudzyslowia ;-)

--- ptv.sh
+++ ptv.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 function wyborProgramu {
-	data=`date +"%Y-%m-%d"`
+	data=`date +"%Y-%m-%d"`;
 	dialog --menu "Wybierz program:" 0 0 0 1 "TVP-1" 2 "TVP-2" 5 "Polsat" 17 "TVN" 2> "wybor.tmp";
-	wybor=`cat "wybor.tmp`
-	pobierzProgram $wybor $data
+	wybor=`cat "wybor.tmp"`;
+	rm "wybor.tmp";
+	pobierzProgram $wybor $data;
 }
 
 function pobierzProgram {
-	wget -O "program.tmp" "http://tv.wp.pl/id,$1,d,$2,all,1,mprogramy.html";
+	wget -O "program.tmp" "http://tv.wp.pl/id,$1,d,$2,all,1,mprogramy.html" 2> /dev/null;
 	wyswietlenieProgramu "program.tmp";
 	rm "program.tmp";
 }
@@ -19,7 +20,7 @@ function wyswietlenieProgramu {
 
 
 dialog --msgbox "Witaj w programie TV" 0 0;
-
+wyborProgramu;
 
 
 
