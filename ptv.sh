b923577901e715f87ce94b0693bbba5c22e2984c
Natalia Kospat Pawłowska <natalia@natalia-VirtualBox.(none)>
2013-01-17 13:19:18 +0100

    mala poprawka nr 2

--- ptv.sh
+++ ptv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+dialog --menu "Wybierz program:" 0 0 0 1 "TVP-1" 2 "TVP-2" 5 "Polsat" 17 "TVN"
 
 wget -O "program.tmp" "http://tv.wp.pl/id,1,d,2013-01-17,all,1,mprogramy.html"
 
-dialog --menu "Wybierz program:" 0 0 0 1 "TVP-1" 2 "TVP-2" 5 "Polsat" 17 "TVN"
+
